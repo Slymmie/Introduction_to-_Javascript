f65eea7fe2914f1bebc6cce3c463f25d3d55bfff
Slymmie <mikel7vin@gmail.com>
2022-06-24 23:43:50 +0100

    first commit

---
+++ IntroToJavascript.js
@@ -0,0 +1,6 @@
+function IntroductiontoJavascript() {
+    console.log('Name : Teniola Michael Oluwasanu');
+    console.log('Height : 180cm');
+    console.log('Country : Nigeria');
+}
+IntroductiontoJavascript();
